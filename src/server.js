34679d6d3d176e17f60804005a3610bd15ab711f
HectorGarcia01 <hg2779948@gmail.com>
2024-06-27 00:53:27 -0600

    Configuración del puerto del servidor

--- src/server.js
+++ src/server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 
+//Inicializaciones
 const app = express();
+
+//Configuraciones
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+
+
+//Variables Globales
+
+
+//Rutas
+
+
+//Archivos Estáticos
+
 
 module.exports = app;
