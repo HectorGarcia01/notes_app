63432fe08f34ef99575ea8f4fe2710b4377e4118
HectorGarcia01 <hg2779948@gmail.com>
2024-06-27 01:10:11 -0600

    Configuraciones en la ruta de los archivos públicos y de las vistas, configuración del middleware para el análisis de datos de formularios para el manejo de objetos json

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 //Inicializaciones
 const app = express();
 
 //Configuraciones
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
 
 //Middlewares
-
+app.use(express.urlencoded({ extended: false }));
 
 //Variables Globales
 
@@ -16,6 +18,6 @@ app.set('port', process.env.PORT || 3000);
 
 
 //Archivos Estáticos
-
+app.set(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
