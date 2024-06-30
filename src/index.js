7408a9be615c5d54f1582fa250725c769e6055fb
HectorGarcia01 <hg2779948@gmail.com>
2024-06-30 01:13:41 -0600

    Importación de variables de entorno y configuración de bd

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const app = require('./server');
+require('./database/db');
 
 app.listen(app.get('port'), () =>{
     console.log("Servidor inicializado en el puerto ", app.get('port'));
