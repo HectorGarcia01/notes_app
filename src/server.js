c302585f7cebf8603d736002b48a1003eb942165
HectorGarcia01 <hg2779948@gmail.com>
2024-06-30 00:11:05 -0600

    Se cambió el método set por el método use para el manejo de los archivos estáticos

--- src/server.js
+++ src/server.js
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
 });
 
 //Archivos Estáticos
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
