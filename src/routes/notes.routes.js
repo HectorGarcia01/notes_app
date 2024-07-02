d2e9d2ddced4361878c10311170cde36d68880c6
HectorGarcia01 <hg2779948@gmail.com>
2024-07-02 01:26:00 -0600

    Configuración de las rutas para la gestión de notas

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const router = Router();
+
+const { 
+    renderNoteForm, 
+    createNewNote,
+    renderNotes,
+    renderEditForm,
+    updateNote,
+    deleteNote
+} = require('../controllers/notes.controller');
+
+//Nueva nota
+router.get('/notes/add', renderNoteForm);
+router.post('/notes/new-note', createNewNote);
+
+//Obtener todas las notas
+router.get('/notes', renderNotes);
+
+//Editar notas
+router.get('/notes/edit/:id', renderEditForm);
+router.put('/notes/edit/:id', updateNote);
+
+//Eliminar notas
+router.delete('/notes/delete/:id', deleteNote);
+
+module.exports = router;
