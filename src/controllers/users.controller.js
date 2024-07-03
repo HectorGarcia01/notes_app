c73ad2cd3bfcd7b7bdf8fb6b382f9cfecf26c4d6
HectorGarcia01 <hg2779948@gmail.com>
2024-07-03 01:06:12 -0600

    Creación de la función para renderizar el formulario de registro e inicio de sesión y la función para registrar un nuevo usuario a la bd

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,46 @@
+const User = require('../models/User');
+const usersCtrl = {};
+
+usersCtrl.renderSignUpForm = (req, res) => {
+    res.render('users/signup');
+};
+
+usersCtrl.signUp = async (req, res) => {
+    const errors = [];
+    const { name, email, password, confirm_password } = req.body;
+
+    if (password != confirm_password) {
+        errors.push({ text: 'Las contraseñas no coinciden.' });
+    }
+
+    if (password.length < 8) {
+        errors.push({ text: 'La contraseña debe de tener un mínimo de 8 caracteres.' });
+    }
+
+    if (errors.length > 0) {
+        res.render('users/signup', { 
+            errors,
+            name,
+            email
+        });
+    } else {
+        const emailUser = await User.findOne({ email });
+
+        if (emailUser) {
+            req.flash('error_msg', 'El correo ya está en uso.');
+            res.redirect('/users/signup');
+        } else {
+            const newUser = new User({ name, email, password });
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash('success_msg', 'Se ha registrado con éxito.');
+            res.redirect('/users/signin');
+        }
+    }
+};
+
+usersCtrl.renderSignInForm = (req, res) => {
+    res.render('users/signin');
+};
+
+module.exports = usersCtrl;
