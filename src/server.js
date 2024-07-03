987e3a09c91ccba25b596e0ff22a32f152afff08
HectorGarcia01 <hg2779948@gmail.com>
2024-07-03 00:57:02 -0600

    Configuración del middleware de session y flash, además se declararon dos variables globales para mensajes de alertas

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const { create } = require('express-handlebars');
 const path = require('path');
 const morgan = require('morgan');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 //Inicializaciones
 const app = express();
@@ -27,13 +29,24 @@ app.set('view engine', '.hbs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(session({
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
 
 //Variables Globales
-
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 //Rutas
 app.use(require('./routes/index.routes'));
 app.use(require('./routes/notes.routes'));
+app.use(require('./routes/users.routes'));
 
 //Archivos Estáticos
 app.use(express.static(path.join(__dirname, 'public')));
