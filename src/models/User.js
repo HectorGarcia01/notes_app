84d51cefb9d4fbb50e700da92dd6e56a91979144
HectorGarcia01 <hg2779948@gmail.com>
2024-07-07 00:59:17 -0600

    Agregando la propiedad trim para todos los campos del modelo User y la propiedad lowercase para email

--- src/models/User.js
+++ src/models/User.js
@@ -4,16 +4,20 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true,
+        lowercase: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     }
 }, {
     timestamps: true
