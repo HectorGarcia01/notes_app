4a04dec3c2e0fe30c5a68092bb26bb9e96a5c895
HectorGarcia01 <hg2779948@gmail.com>
2024-07-07 00:39:00 -0600

    se agregó el middleware de validación a la ruta para crear un usuario y para iniciar sesión

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
+const {isValidated } = require('../middlewares/validate');
+const { createUserSchema, signInSchema } = require('../schemas/users.schema');
 const {
     renderSignUpForm,
     signUp,
@@ -12,11 +14,11 @@ const {
 
 //Nuevo usuario
 router.get('/users/signup', renderSignUpForm);
-router.post('/users/signup', signUp);
+router.post('/users/signup', isValidated(createUserSchema, 'users/signup'), signUp);
 
 //Iniciar sesión
 router.get('/users/signin', renderSignInForm);
-router.post('/users/signin', signIn);
+router.post('/users/signin', isValidated(signInSchema, 'users/signin'), signIn);
 
 //Cerrar sesión
 router.get('/users/logout', logout);
