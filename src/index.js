dd9b7f303886ae88d58913a5a0536d610e458118
HectorGarcia01 <hg2779948@gmail.com>
2024-07-07 01:02:14 -0600

    Convirtiendo el servidor http a https

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const app = require('./server');
 require('./database/db');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
 
-app.listen(app.get('port'), () =>{
+const sslPath = path.join(__dirname, 'utils/ssl');
+
+https.createServer({
+    cert: fs.readFileSync(path.join(sslPath, 'server.cer')),
+    key: fs.readFileSync(path.join(sslPath, 'server.key'))
+}, app).listen(app.get('port'), () =>{
     console.log("Servidor inicializado en el puerto ", app.get('port'));
 });
