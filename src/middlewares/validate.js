30c8a97253755ed2251fd0e796f4c19ae7cd3cef
HectorGarcia01 <hg2779948@gmail.com>
2024-07-07 02:25:44 -0600

    Modificación del middleware de validación, se implementó un nuevo parámetro para distinguir el tipo de registro y así poder devolver algunos datos sin resetear el formulario

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,12 +1,23 @@
+const { renderSignUpForm } = require('../controllers/users.controller');
+const { renderEditForm } = require('../controllers/notes.controller');
 const middleware = {};
 
-middleware.isValidated = (schema, redirectUrl) => {
+middleware.isValidated = (schema, redirectUrl, data) => {
     return async (req, res, next) => {
         try {
             await schema.validateAsync(req.body, { abortEarly: false });
             next();
         } catch (error) {
             const errors = error.details.map(err => ({ text: err.message }));
+            
+            if (data === 'addUser') {
+                return renderSignUpForm(req, res, errors, req.body.name, req.body.email);
+            }
+
+            if (data === 'notes') {
+                return renderEditForm(req, res, errors, 'Datos restaurados exitosamente.');
+            }
+
             res.render(redirectUrl, { errors });
         }
     }
